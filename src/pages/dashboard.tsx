383fcc6f03fa04645e187e3eb5c1d827effcfcd4
Suman Mondal <suman.mondal@techriskpartners.com>
2024-03-11 12:41:07 +0530

    Added activity card

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,33 +1,31 @@
-import {  DynamicPage } from "@ui5/webcomponents-react";
+import { DynamicPage } from "@ui5/webcomponents-react";
 import DashboardCards from "../components/DashboardCards";
 import { dasboardCardData } from "../lib/dashboardCardData";
 import DashboardDatePicker from "../components/DashboardDatePicker";
+import ActivityCard from "../components/ActivityCard";
+
 const Dashboard = () => {
 	return (
-	
 		<DynamicPage
-		headerContentPinnable={false}
-		showHideHeaderButton={false}
-		headerContent={
-			<DashboardDatePicker/>
-		}
-		>
-		
-
+			headerContentPinnable={false}
+			showHideHeaderButton={false}
+			headerContent={<DashboardDatePicker />}>
 			{dasboardCardData.map((card, index) => {
 				return (
 					<DashboardCards
-						key={index} 
-						header={card.header} 
-						description={card.description}/>
+						key={index}
+						header={card.header}
+						description={card.description}
+					/>
 				);
 			})}
-			
-      
-		</DynamicPage>
-		
 
-		)
+			<ActivityCard
+				title="Incomplete Customer Master"
+				description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, molestiae."
+			/>
+		</DynamicPage>
+	);
 };
 
 export default Dashboard;
