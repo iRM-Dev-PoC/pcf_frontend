68646c4dc497474d50a6364f955083331d924e12
pritys02 <prity.singh@techriskpartners.com>
2024-03-13 11:56:11 +0530

    risk factor

--- src/components/RiskFactor.tsx
+++ src/components/RiskFactor.tsx
@@ -1,29 +1,30 @@
-import { Card, CardHeader, Icon, List } from "@ui5/webcomponents-react"
+import { Card, CardHeader, Icon, } from "@ui5/webcomponents-react"
 import { RadialChart } from "@ui5/webcomponents-react-charts"
-// import {RadialChart} from "@ui5/webcomponents-react"
-
 const RiskFactor = () => {
   return (
     <Card 
-    header={<CardHeader avatar={<Icon name="person-placeholder" />} status="3 of 5" subtitleText="Direct Reports" titleText="TeamSpace"/>}
-  style={{
-    width: '20%',
-  }}
+    header={<CardHeader avatar={<Icon name="person-placeholder" />}  titleText="Risk Factor"/>}
+    style={{width :"30%"}}
     >
-     <List>
-     <RadialChart
-     color="#f0ab00"
-     displayValue="50%"
-     onClick={function _a(){}}
-     onDataPointClick={function _a(){}}
-     value={50}
-     />
-   
-    </List>
-    </Card>
-
     
+    <RadialChart
+  chartConfig={{innerRadius:"145"}}
+  displayValue="50%"
+  onClick={function _a(){}}
+  onDataPointClick={function _a(){}}
+  value={50}
+/>
+    </Card>   
   )
 }
 
-export default RiskFactor
+export default RiskFactor;
+
+
+
+
+
+
+
+
+
