4142172a2a4c6015b2a526ad7eade4482cf0a1d5
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-23 12:01:49 +0530

    Revert "chore: removed unused code"
    This reverts commit 9e681504b28782fc0d9f12775aa2ada4bc4f2c64.

--- src/components/RiskCard.tsx
+++ src/components/RiskCard.tsx
@@ -14,6 +14,31 @@ type RiskCardProps = {
 
 const RiskCard = ({ header, risk, description, onClick }: RiskCardProps) => {
     return (
+        // <Card style={{ width: "32rem" }} className="m-2.5">
+        //     <CardHeader
+        //         titleText={header}
+        //         onClick={onClick}
+        //         action={
+        //             <Badge
+        //                 colorScheme={
+        //                     Number(risk) >= 0 && Number(risk) <= 30
+        //                         ? "8"
+        //                         : Number(risk) > 30 && Number(risk) <= 70
+        //                           ? "1"
+        //                           : "2"
+        //                 }
+        //             >
+        //                 Risk : {risk}
+        //             </Badge>
+        //         }
+        //         interactive={true}
+        //     />
+        //     <List>
+        //         <StandardListItem className="pointer-events-none">
+        //             {description}
+        //         </StandardListItem>
+        //     </List>
+        // </Card>
         <Card className="m-2.5 max-w-[32rem]">
             <CardHeader
                 titleText={header}
