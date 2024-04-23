5acba251b974019b109d07ae196e460ee9560af2
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-23 12:20:50 +0530

    fix: fixed issue with icon not showing

--- src/components/RiskCard.tsx
+++ src/components/RiskCard.tsx
@@ -14,31 +14,7 @@ type RiskCardProps = {
 
 const RiskCard = ({ header, risk, description, onClick }: RiskCardProps) => {
     return (
-        // <Card style={{ width: "32rem" }} className="m-2.5">
-        //     <CardHeader
-        //         titleText={header}
-        //         onClick={onClick}
-        //         action={
-        //             <Badge
-        //                 colorScheme={
-        //                     Number(risk) >= 0 && Number(risk) <= 30
-        //                         ? "8"
-        //                         : Number(risk) > 30 && Number(risk) <= 70
-        //                           ? "1"
-        //                           : "2"
-        //                 }
-        //             >
-        //                 Risk : {risk}
-        //             </Badge>
-        //         }
-        //         interactive={true}
-        //     />
-        //     <List>
-        //         <StandardListItem className="pointer-events-none">
-        //             {description}
-        //         </StandardListItem>
-        //     </List>
-        // </Card>
+        
         <Card className="m-2.5 max-w-[32rem]">
             <CardHeader
                 titleText={header}
@@ -58,7 +34,7 @@ const RiskCard = ({ header, risk, description, onClick }: RiskCardProps) => {
                 }
                 interactive={true}
             />
-            <div className=" p-4">
+            <div className="p-4">
                 <ExpandableText maxCharacters={50}>
                     {description}
                 </ExpandableText>
