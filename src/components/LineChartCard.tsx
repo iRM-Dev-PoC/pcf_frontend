af5c9da2eb9ca968b50b148fb6d6ef41a9576685
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-25 12:09:53 +0530

    added allicon import and added a height and width on charts

--- src/components/LineChartCard.tsx
+++ src/components/LineChartCard.tsx
@@ -3,29 +3,29 @@ import { LineChart } from "@ui5/webcomponents-react-charts";
 import { lineChartData } from "../lib/lineChartData";
 
 const LineChartCard = () => {
-    return (
-        <Card>
-            <LineChart
-                dataset={lineChartData}
-                dimensions={[
-                    {
-                        accessor: "name",
-                    },
-                ]}
-                measures={[
-                    {
-                        accessor: "users",
-                    },
-                    {
-                        accessor: "sessions",
-                    },
-                    {
-                        accessor: "volume",
-                    },
-                ]}
-            />
-        </Card>
-    );
+	return (
+		<Card style={{ width: "100%", height: "100%", aspectRatio: "1" }}>
+			<LineChart
+				dataset={lineChartData}
+				dimensions={[
+					{
+						accessor: "name",
+					},
+				]}
+				measures={[
+					{
+						accessor: "users",
+					},
+					{
+						accessor: "sessions",
+					},
+					{
+						accessor: "volume",
+					},
+				]}
+			/>
+		</Card>
+	);
 };
 
 export default LineChartCard;
--- src/main.tsx
+++ src/main.tsx
@@ -14,52 +14,57 @@ import { ProductSwitchProvider } from "./context/productSwitchContext.tsx";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "@ui5/webcomponents-icons/dist/menu2.js"
-import "@ui5/webcomponents-icons/dist/log.js"
-import "@ui5/webcomponents-icons/dist/full-screen.js"
-import "@ui5/webcomponents-icons/dist/wrench.js"
-import "@ui5/webcomponents-icons/dist/customer-order-entry.js"
-import "@ui5/webcomponents-icons/dist/add-employee.js"
-import "@ui5/webcomponents-icons/dist/permission.js"
-import "@ui5/webcomponents-icons/dist/key.js"
-import "@ui5/webcomponents-icons/dist/home.js"
-import "@ui5/webcomponents-icons/dist/customer-and-contacts.js"
-import "@ui5/webcomponents-icons/dist/drop-down-list.js"
-import "@ui5/webcomponents-icons/dist/manager-insight.js"
-import "@ui5/webcomponents-icons/dist/role.js"
-import "@ui5/webcomponents-icons/dist/provision.js"
-import "@ui5/webcomponents-icons/dist/flight.js"
-import "@ui5/webcomponents-icons/dist/contacts.js"
-import "@ui5/webcomponents-icons/dist/business-objects-experience.js"
-import "@ui5/webcomponents-icons/dist/activity-individual.js"
-import "@ui5/webcomponents-icons/dist/process.js"
-import "@ui5/webcomponents-icons/dist/person-placeholder.js"
-import "@ui5/webcomponents-icons/dist/delete.js"
+import "@ui5/webcomponents-icons/dist/menu2.js";
+import "@ui5/webcomponents-icons/dist/log.js";
+import "@ui5/webcomponents-icons/dist/full-screen.js";
+import "@ui5/webcomponents-icons/dist/wrench.js";
+import "@ui5/webcomponents-icons/dist/customer-order-entry.js";
+import "@ui5/webcomponents-icons/dist/add-employee.js";
+import "@ui5/webcomponents-icons/dist/permission.js";
+import "@ui5/webcomponents-icons/dist/key.js";
+import "@ui5/webcomponents-icons/dist/home.js";
+import "@ui5/webcomponents-icons/dist/customer-and-contacts.js";
+import "@ui5/webcomponents-icons/dist/drop-down-list.js";
+import "@ui5/webcomponents-icons/dist/manager-insight.js";
+import "@ui5/webcomponents-icons/dist/role.js";
+import "@ui5/webcomponents-icons/dist/provision.js";
+import "@ui5/webcomponents-icons/dist/flight.js";
+import "@ui5/webcomponents-icons/dist/contacts.js";
+import "@ui5/webcomponents-icons/dist/business-objects-experience.js";
+import "@ui5/webcomponents-icons/dist/activity-individual.js";
+import "@ui5/webcomponents-icons/dist/process.js";
+import "@ui5/webcomponents-icons/dist/person-placeholder.js";
+import "@ui5/webcomponents-icons/dist/delete.js";
+import "@ui5/webcomponents-icons/dist/exit-full-screen.js";
+import "@ui5/webcomponents-icons/Assets.js";
+import "@ui5/webcomponents-icons/AllIcons.js";
+import "@ui5/webcomponents/Assets.js";
+import "@ui5/webcomponents-localization/Assets.js";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <ErrorBoundary fallback={<ErrorPage />}>
-            <QueryClientProvider client={queryClient}>
-                <ThemeProvider>
-                    <BrowserRouter>
-                        <SidebarProvider>
-                            <PathProvider>
-                                <CurrentURLProvider>
-                                    <ProductSwitchProvider>
-                                        <Suspense fallback={<Loading />}>
-                                            <App />
-                                            <Toaster />
-                                            <ReactQueryDevtools />
-                                        </Suspense>
-                                    </ProductSwitchProvider>
-                                </CurrentURLProvider>
-                            </PathProvider>
-                        </SidebarProvider>
-                    </BrowserRouter>
-                </ThemeProvider>
-            </QueryClientProvider>
-        </ErrorBoundary>
-    </StrictMode>
+	<StrictMode>
+		<ErrorBoundary fallback={<ErrorPage />}>
+			<QueryClientProvider client={queryClient}>
+				<ThemeProvider>
+					<BrowserRouter>
+						<SidebarProvider>
+							<PathProvider>
+								<CurrentURLProvider>
+									<ProductSwitchProvider>
+										<Suspense fallback={<Loading />}>
+											<App />
+											<Toaster />
+											<ReactQueryDevtools />
+										</Suspense>
+									</ProductSwitchProvider>
+								</CurrentURLProvider>
+							</PathProvider>
+						</SidebarProvider>
+					</BrowserRouter>
+				</ThemeProvider>
+			</QueryClientProvider>
+		</ErrorBoundary>
+	</StrictMode>
 );
