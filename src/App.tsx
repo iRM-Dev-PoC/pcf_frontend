cca97fdf480117a75986d8170a004e2d548560e7
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-29 12:06:29 +0530

    removed ControlAttribute

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import Role from "./pages/roles";
 import AddUsers from "./pages/users";
 import SignIn from "./pages/signIn";
 import Home from "./pages/home";
-import ControlAttribute from "./pages/controlAttribute";
 import ControlFamily from "./pages/controlFamily";
 import ControlLogic from "./pages/controlLogic";
 import Report from "./pages/report";
@@ -34,130 +33,150 @@ import { User } from "./utils/types";
 import ReportCheckPointMapping from "./pages/reportCheckPointMapping";
 
 const App = () => {
-	const [, setTheme] = useState("sap_horizon");
+    const [, setTheme] = useState("sap_horizon");
 
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [user, setUser] = useState<User | undefined>(undefined);
-	const [isForgotPassword, setIsForgotPassword] = useState(false);
-	const { isSwitchProduct } = useSwitchProduct();
-	const [product, setProduct] = useState("Process Control Flow");
-	const [isSoD, setIsSoD] = useState(false);
-	const path = useCurrentPath();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState<User | undefined>(undefined);
+    const [isForgotPassword, setIsForgotPassword] = useState(false);
+    const { isSwitchProduct } = useSwitchProduct();
+    const [product, setProduct] = useState("Process Control Flow");
+    const [isSoD, setIsSoD] = useState(false);
+    const path = useCurrentPath();
 
-	useEffect(() => {
-		const userData = localStorage.getItem("userData");
-		if (userData) {
-			setIsLoggedIn(true);
-			const parsedUserData = JSON.parse(userData);
-			setUser(parsedUserData);
-		}
-		if (path.includes("/sod")) {
-			setIsSoD(true);
-			setProduct("Segregation of Duties");
-		}
-	}, [isLoggedIn, path]);
+    useEffect(() => {
+        const userData = localStorage.getItem("userData");
+        if (userData) {
+            setIsLoggedIn(true);
+            const parsedUserData = JSON.parse(userData);
+            setUser(parsedUserData);
+        }
+        if (path.includes("/sod")) {
+            setIsSoD(true);
+            setProduct("Segregation of Duties");
+        }
+    }, [isLoggedIn, path]);
 
-	return (
-		<div
-			className={`overflow-hidden`}
-			style={{
-				backgroundColor: `color-mix(in srgb, black 4%, var(--sapBackgroundColor))`,
-				gridTemplateRows: "auto 1fr",
-				gridTemplateColumns: "auto 1fr",
-			}}>
-			{isForgotPassword ? (
-				<ForgetPassword />
-			) : !isLoggedIn ? (
-				<SignIn
-					setIsLoggedIn={setIsLoggedIn}
-					setIsForgotPassword={setIsForgotPassword}
-				/>
-			) : isSwitchProduct ? (
-				<ProductSelection />
-			) : (
-				<Suspense fallback={<Loading />}>
-					<Navbar
-						companyName="TRP Global"
-						productName={product}
-						isNotifiction={true}
-						notificationCount="10"
-						companyLogo={companyLogo}
-						fName={user ? user.fName : ""}
-						lName={user ? user.lName : ""}
-						themeSwitch={setTheme}
-					/>
+    return (
+        <div
+            className={`overflow-hidden`}
+            style={{
+                backgroundColor: `color-mix(in srgb, black 4%, var(--sapBackgroundColor))`,
+                gridTemplateRows: "auto 1fr",
+                gridTemplateColumns: "auto 1fr",
+            }}
+        >
+            {isForgotPassword ? (
+                <ForgetPassword />
+            ) : !isLoggedIn ? (
+                <SignIn
+                    setIsLoggedIn={setIsLoggedIn}
+                    setIsForgotPassword={setIsForgotPassword}
+                />
+            ) : isSwitchProduct ? (
+                <ProductSelection />
+            ) : (
+                <Suspense fallback={<Loading />}>
+                    <Navbar
+                        companyName="TRP Global"
+                        productName={product}
+                        isNotifiction={true}
+                        notificationCount="10"
+                        companyLogo={companyLogo}
+                        fName={user ? user.fName : ""}
+                        lName={user ? user.lName : ""}
+                        themeSwitch={setTheme}
+                    />
 
-					<FlexBox
-						style={{
-							height: "91.95vh",
-							marginTop: "0.50rem",
-							columnGap: "0.50rem",
-							marginRight: "0.50rem",
-							marginBottom: "0.3rem",
-							borderRadius: "0.5rem",
-						}}>
-						{isSoD ? (
-							<SideNavbar items={sodRoutes} />
-						) : (
-							<SideNavbar items={routes} />
-						)}
+                    <FlexBox
+                        style={{
+                            height: "91.95vh",
+                            marginTop: "0.50rem",
+                            columnGap: "0.50rem",
+                            marginRight: "0.50rem",
+                            marginBottom: "0.3rem",
+                            borderRadius: "0.5rem",
+                        }}
+                    >
+                        {isSoD ? (
+                            <SideNavbar items={sodRoutes} />
+                        ) : (
+                            <SideNavbar items={routes} />
+                        )}
 
-						<Suspense fallback={<Loading />}>
-							<Routes>
-								<Route path="/" element={<Home />} />
+                        <Suspense fallback={<Loading />}>
+                            <Routes>
+                                <Route path="/" element={<Home />} />
 
-								{/* Master routes */}
+                                {/* Master routes */}
+                                <Route
+                                    path="/pcf/master/controlFamily"
+                                    element={<ControlFamily />}
+                                />
+                                <Route
+                                    path="/pcf/master/controlLogic"
+                                    element={<ControlLogic />}
+                                />
+                                <Route
+                                    path="/pcf/master/report"
+                                    element={<Report />}
+                                />
+                                <Route
+                                    path="/pcf/master/typeOfControl"
+                                    element={<TypeOfControl />}
+                                />
+                                <Route
+                                    path="/pcf/master/checkpoint"
+                                    element={<CheckPoint />}
+                                />
+                                <Route
+                                    path="/pcf/master/reportCheckpointMapping"
+                                    element={<ReportCheckPointMapping />}
+                                />
 
-								<Route
-									path="/pcf/master/controlAttribute"
-									element={<ControlAttribute />}
-								/>
-								<Route
-									path="/pcf/master/controlFamily"
-									element={<ControlFamily />}
-								/>
-								<Route
-									path="/pcf/master/controlLogic"
-									element={<ControlLogic />}
-								/>
-								<Route path="/pcf/master/report" element={<Report />} />
-								<Route
-									path="/pcf/master/typeOfControl"
-									element={<TypeOfControl />}
-								/>
-								<Route path="/pcf/master/checkpoint" element={<CheckPoint />} />
-								<Route
-									path="/pcf/master/reportCheckpointMapping"
-									element={<ReportCheckPointMapping />}
-								/>
+                                {/* Dataload routes */}
+                                <Route
+                                    path="/pcf/dataLoad"
+                                    element={<DataLoad />}
+                                />
 
-								{/* Dataload routes */}
-								<Route path="/pcf/dataLoad" element={<DataLoad />} />
+                                {/* Configuration routes */}
+                                <Route
+                                    path="/pcf/config/roles"
+                                    element={<Role />}
+                                />
+                                <Route
+                                    path="/pcf/config/addUsers"
+                                    element={<AddUsers />}
+                                />
 
-								{/* Configuration routes */}
-								<Route path="/pcf/config/roles" element={<Role />} />
-								<Route path="/pcf/config/addUsers" element={<AddUsers />} />
+                                <Route
+                                    path="/pcf/config/subModule"
+                                    element={<SubModule />}
+                                />
 
-								<Route path="/pcf/config/subModule" element={<SubModule />} />
+                                <Route
+                                    path="/pcf/config/moduleMaster"
+                                    element={<ModuleMaster />}
+                                />
 
-								<Route
-									path="/pcf/config/moduleMaster"
-									element={<ModuleMaster />}
-								/>
+                                <Route
+                                    path="/sod/"
+                                    element={<SoDDashboard />}
+                                />
 
-								<Route path="/sod/" element={<SoDDashboard />} />
-
-								<Route
-									path="/resetPassword"
-									element={<ResetPassword changePassword={true} />}
-								/>
-							</Routes>
-						</Suspense>
-					</FlexBox>
-				</Suspense>
-			)}
-		</div>
-	);
+                                <Route
+                                    path="/resetPassword"
+                                    element={
+                                        <ResetPassword changePassword={true} />
+                                    }
+                                />
+                            </Routes>
+                        </Suspense>
+                    </FlexBox>
+                </Suspense>
+            )}
+        </div>
+    );
 };
 
 export default App;
--- src/lib/routedata.ts
+++ src/lib/routedata.ts
@@ -1,96 +1,91 @@
 const routes = [
-	{
-		text: "Home",
-		icon: "home",
-		path: "/",
-	},
-	{
-		text: "Data Load",
-		icon: "upload-to-cloud",
-		path: "/pcf/dataLoad",
-	},
-	{
-		text: "Master",
-		icon: "drop-down-list",
-		path: "/pcf/master",
-		subItems: [
-			{
-				text: "Type Of Control",
-				icon: "permission",
-				path: "/typeOfControl",
-			},
-			{
-				text: "Control Family",
-				icon: "customer-and-contacts",
-				path: "/controlFamily",
-			},
-			{
-				text: "Control Attribute",
-				icon: "customer-order-entry",
-				path: "/controlAttribute",
-			},
-			{
-				text: "Report",
-				icon: "manager-insight",
-				path: "/report",
-			},
-			{
-				text: "Control Logic",
-				icon: "wrench",
-				path: "/controlLogic",
-			},
-			{
-				text: "Check-Point Master",
-				icon: "flag-2",
-				path: "/checkpoint",
-			},
-			{
-				text: "Report Check-Point Mapping",
-				icon: "org-chart",
-				path: "/reportCheckpointMapping",
-			},
-		],
-	},
-	{
-		text: "Configuration",
-		icon: "wrench",
-		path: "/pcf/config",
-		subItems: [
-			{
-				text: "Add Roles",
-				icon: "role",
-				path: "/roles",
-			},
-			{
-				text: "Add Users",
-				icon: "add-employee",
-				path: "/addUsers",
-			},
-			{
-				text: "Module Master",
-				icon: "grid",
-				path: "/moduleMaster",
-			},
-			{
-				text: "Sub-Module Master",
-				icon: "provision",
-				path: "/subModule",
-			},
-		],
-	},
+    {
+        text: "Home",
+        icon: "home",
+        path: "/",
+    },
+    {
+        text: "Data Load",
+        icon: "upload-to-cloud",
+        path: "/pcf/dataLoad",
+    },
+    {
+        text: "Master",
+        icon: "drop-down-list",
+        path: "/pcf/master",
+        subItems: [
+            {
+                text: "Type Of Control",
+                icon: "permission",
+                path: "/typeOfControl",
+            },
+            {
+                text: "Control Family",
+                icon: "customer-and-contacts",
+                path: "/controlFamily",
+            },
+            {
+                text: "Report",
+                icon: "manager-insight",
+                path: "/report",
+            },
+            {
+                text: "Control Logic",
+                icon: "wrench",
+                path: "/controlLogic",
+            },
+            {
+                text: "Check-Point Master",
+                icon: "flag-2",
+                path: "/checkpoint",
+            },
+            {
+                text: "Report Check-Point Mapping",
+                icon: "org-chart",
+                path: "/reportCheckpointMapping",
+            },
+        ],
+    },
+    {
+        text: "Configuration",
+        icon: "wrench",
+        path: "/pcf/config",
+        subItems: [
+            {
+                text: "Add Roles",
+                icon: "role",
+                path: "/roles",
+            },
+            {
+                text: "Add Users",
+                icon: "add-employee",
+                path: "/addUsers",
+            },
+            {
+                text: "Module Master",
+                icon: "grid",
+                path: "/moduleMaster",
+            },
+            {
+                text: "Sub-Module Master",
+                icon: "provision",
+                path: "/subModule",
+            },
+        ],
+    },
 ];
 
 const sodRoutes = [
-	{
-		text: "Home",
-		icon: "home",
-		path: "/sod/",
-	},
-	{
-		text: "Test",
-		icon: "feed",
-		path: "/sod/test",
-	},
+    {
+        text: "Home",
+        icon: "home",
+        path: "/sod/",
+    },
+    {
+        text: "Test",
+        icon: "feed",
+        path: "/sod/test",
+    },
 ];
 
 export { sodRoutes, routes };
