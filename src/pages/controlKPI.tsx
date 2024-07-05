8abce372c1def10913591caf1d971e778f1f9ffa
Prity Singh <prity.singh@techriskpartners.com>
2024-07-05 12:39:14 +0530

    added routes for KPI controls and dynamic page for KPI control

--- src/pages/controlKPI.tsx
+++ src/pages/controlKPI.tsx
@@ -1,14 +1,31 @@
-import { DynamicPage } from "@ui5/webcomponents-react"
 import "@/css/dynamicPage.css";
+import {
+    DynamicPage,
+    DynamicPageTitle,
+    MessageStrip,
+    Title,
+} from "@ui5/webcomponents-react";
+
+import { ThemingParameters } from "@ui5/webcomponents-react-base";
 
 const controlKPI = () => {
-  return (
-      <DynamicPage
-          className="dynamicPage"
-          showHideHeaderButton={false}
-          headerContentPinnable={false}
-      ></DynamicPage>
-  );
-}
+    return (
+        <DynamicPage
+            className="dynamicPage"
+            headerTitle={
+                <DynamicPageTitle
+                    header={<Title>KPI - Controls</Title>}
+                    snappedContent={<MessageStrip>xyz</MessageStrip>}
+                ></DynamicPageTitle>
+            }
+            style={{
+                borderRadius: ThemingParameters.sapButton_BorderCornerRadius,
+            }}
+            showHideHeaderButton={false}
+            headerContentPinnable={false}
+        ></DynamicPage>
+    );
+};
 
 export default controlKPI;
+
